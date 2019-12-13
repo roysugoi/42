e0e8bb1f1bc3fc48fb8da95f60fb71af83a925b8
Roy <r.vegasjimenez@gmail.com>
2019-12-13 15:29:05 +0100

    Score: 100.

--- Libft/ft_atoi.c
+++ Libft/ft_atoi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rvegas-j <rvegas-j@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/11 16:14:53 by rvegas-j          #+#    #+#             */
-/*   Updated: 2019/11/16 16:55:09 by rvegas-j         ###   ########.fr       */
+/*   Updated: 2019/12/13 15:28:58 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ int	ft_atoi(const char *str)
 {
 	int					i;
 	long				neg;
-	unsigned long long	num;
+	unsigned long long	n;
 
 	i = 0;
 	neg = 1;
-	num = 0;
+	n = 0;
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
 		i++;
 	if ((str[i] == '+' || str[i] == '-'))
@@ -33,8 +33,8 @@ int	ft_atoi(const char *str)
 	{
 		if (i >= 19)
 			return (neg == -1 ? 0 : -1);
-		num = num * 10 + str[i] - '0';
+		n = n * 10 + str[i] - '0';
 		i++;
 	}
-	return (num * neg);
+	return (n * neg);
 }
